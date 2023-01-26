c7949c484c27b2e5293d06db39ad2393f894c7a5
Askioe <rolenlouie123@gmail.com>
2023-01-25 17:48:10 -0800

    Just some minor edits

--- main.py
+++ main.py
@@ -5,11 +5,38 @@ from time import sleep
 import datetime
 
 
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1067969743404945529/Lpbsj2P254yHgNVpRV19jAn9HpydJl2KN-8uctM57mlZVvDuFKdw5KaC8qaeF0KF60Dl')
+webhook = DiscordWebhook(url='')
 
-def find_listings():
+
+def Send_Webhook(title,image,url):
+    embed = DiscordEmbed(title=title, color='03b2f8')
+
+    embed.set_author(name='Ask', url='https://github.com/Askioe', icon_url='https://user-images.githubusercontent.com/58087709/102176189-c4cf6300-3e55-11eb-9331-1c0895b9a736.jpg')
+
+    embed.set_image(url="https://govdeals.com"+image)
+
+
+    embed.set_thumbnail(url="https://govdeals.com"+image)
+
+
+    embed.set_footer(text='Fuck you Glenn')
+
+
+    embed.set_timestamp()
+
+
+    embed.add_embed_field(name='Year: ', value=title[0:4])
+    embed.add_embed_field(name='Quick Link: ', value="https://govdeals.com/"+url)
+
+    webhook.add_embed(embed)
+
+    response = webhook.execute()    
+
+
+
+def find_listings(look_for_charger):
     print("Initialized.")
-    r = requests.get("https://www.govdeals.com/chpvehiclesales")
+    r = requests.get("https://www.govdeals.com/index.cfm?fa=Main.AdvSearchResultsNew&kWord=dodge%20charger&whichForm=vehicle&searchPg=Main")
     #print(r.text)
     bs = soup(r.text, "html.parser")
     try:
@@ -34,36 +61,19 @@ def find_listings():
             title = i.find('a', {"class": "highslide"})['title']
             image = i.find('a', {"class": "highslide"})['href']
             url = i.find('a', {"href": lambda x: x and x.__contains__('index.cfm')})['href']
-            if "Dodge Charger" in i.text:
-                embed = DiscordEmbed(title=title, color='03b2f8')
-                # set author
-                embed.set_author(name='Ask', url='https://github.com/Askioe', icon_url='https://user-images.githubusercontent.com/58087709/102176189-c4cf6300-3e55-11eb-9331-1c0895b9a736.jpg')
-
-                embed.set_image(url="https://govdeals.com"+image)
-
-                # set thumbnail
-                embed.set_thumbnail(url="https://govdeals.com"+image)
-
-                # set footer
-                embed.set_footer(text='Fuck you Glenn')
-
-                # set timestamp (default is now)
-                embed.set_timestamp()
-
-                # add fields to embed
-                embed.add_embed_field(name='Year: ', value=title[0:4])
-                embed.add_embed_field(name='Quick Link: ', value="https://govdeals.com/"+url)
-                # add embed object to webhook
-                webhook.add_embed(embed)
-
-                response = webhook.execute()
+            if look_for_charger:
+                if "Dodge Charger" in i.text:
+                    Send_Webhook(title,image,url)
+                    sleep(.5)
+            else:
+                Send_Webhook(title,image,url)
                 sleep(.5)
         return 0
 
 
 def main():
     while True:
-        find_listings()
+        find_listings(True)
         sleep(60 * 60)
 
 
